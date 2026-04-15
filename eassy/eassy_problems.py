f4963dabcfc96d64d834040cb485337fce2b5205
Elvis Boves <e.boves@gmail.com>
2026-04-15 13:01:26 -0400

    Add new problem to solve Mayority Element

--- eassy/eassy_problems.py
+++ eassy/eassy_problems.py
@@ -247,6 +247,43 @@ def is_palindrome(s):
 
     return True     
     
+# print(is_palindrome(s))
 
-print(is_palindrome(s))
+"""
+###################################################### PROBLEM 6 ######################################################################
 
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 5 * 104
+    -109 <= nums[i] <= 109
+    The input is generated such that a majority element will exist in the array.
+
+ 
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+"""
+
+nums = [3,2,3]
+# nums = [2,2,1,1,1,2,2]
+
+def mayority_num(nums):
+    seen = {}
+    for i in range(len(nums)):
+        print(nums[i])
+
+mayority_num(nums)
