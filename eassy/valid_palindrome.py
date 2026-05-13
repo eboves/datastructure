cf00dcb29447b2f22bb3d8fbca15f15ecbb8ffad
Elvis <e.boves@gmail.com>
2026-05-12 21:16:30 -0400

    finish problem 125 Valid Palindrom

--- eassy/valid_palindrome.py
+++ eassy/valid_palindrome.py
@@ -18,25 +18,39 @@
 # ============================================
 
 
-# INPUT:     
-# OUTPUT:    
-# OPERATION: ___(primary) → ___ → ___
-# PATTERN:   
-# DS:        
+# INPUT: a phrase 
+# OUTPUT:  return a boolean True or False  
+# OPERATION: compare(primary) → return 
+# PATTERN: two pointers different positions, left=0; right=len(s)-1
+# DS: two pointers
 # PLAN:
-#   1. 
-#   2. 
-#   3. 
-#   4. 
-#   5.
+#   1. create left=0; fast=len(s) - 1
+#   2. while left <= fast:
+#      s = s.low()
+#   3. s = ''.join(c for c in s if c.isalnum())
+#   4. if s[left] == s[right]:
+#   5.      left += 1; right -= 1
+#       else:
+#           return False
+#       return True
 
 
 
-s = "A man, a plan, a canal: Panama"
-# s = "race a car"
+# s = "A man, a plan, a canal: Panama"
+s = "race a car"
 
 def is_palindrome(s):
+    s = s.lower()
+    s = ''.join(c for c in s if c.isalnum())
+    left=0
+    right=len(s) - 1
+    while left <= right:
+        if s[left] == s[right]:
+            left += 1
+            right -= 1
+        else:
+            return False
+    return True
 
-    pass
 
-# print(is_palindrome(s))
+print(is_palindrome(s))
