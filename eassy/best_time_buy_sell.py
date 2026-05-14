c65bdcea2aedd266717109031d1f75897aa17462
Elvis Boves <e.boves@gmail.com>
2026-05-14 14:25:17 -0400

    working on best time to buy and sell stocks

--- eassy/best_time_buy_sell.py
+++ eassy/best_time_buy_sell.py
@@ -20,23 +20,44 @@
 # ============================================
 
 
-# INPUT:     
-# OUTPUT:    
-# OPERATION: ___(primary) → ___ → ___
-# PATTERN:   
-# DS:        
+# INPUT: a list of prices  
+# OUTPUT: return profit if there is or 0 if not profit
+# OPERATION: compare(primary) → find → return
+# PATTERN: greedy
+# DS: list     
 # PLAN:
-#   1. 
-#   2. 
-#   3. 
-#   4. 
-#   5.
+#   1. create min_price = prices[0] and max_profit = 0, profit = 0
+#   2. loop through prices
+#   3.      if n+1 < min_price:
+#   4.          min_price = price
+#   5.          profit = min_price - price
+                # if profit > max_profit:
+                #     max_profit = profit
+        #     return max_profit
+        # return 0
+
+
+
+
+
+
 
 prices = [7, 1, 5, 3, 6, 4]
 # prices = [7, 6, 4, 3, 1]
 
 def max_profit(prices):
-  
-    pass
+    min_price = prices[0]
+    max_profit = 0
+    profit = 0
+    for price in range(len(prices)+ 1):
+        if min_price <= prices[price]:
+            print("inside if")
+            min_price = price
+        profit = price - min_price
+        print(profit)
+        if profit > max_profit:
+            max_profit = profit
+        return 0
+    return max_profit
 
-# print(max_profit(prices))
+print(max_profit(prices))
