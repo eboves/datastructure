af3a804d5495df638740ca457b5c25035e0616be
Elvis <e.boves@gmail.com>
2026-05-16 12:12:44 -0400

    started second sliding window problem

--- medium/medium_problems.py
+++ medium/medium_problems.py
@@ -66,6 +66,53 @@ def max_sum_subarray(nums, k):
         max_sum = max(max_sum, current_sum)
     return max_sum
 
-print(max_sum_subarray(nums, k))
+# print(max_sum_subarray(nums, k))
 
 
+# ============================================
+# Average of Each Subarray of Size K
+# Difficulty: Easy
+# Pattern: Sliding Window (Fixed Size)
+# ============================================
+#
+# Given an array of integers and a number k,
+# find the average of each contiguous subarray
+# of size k and return them all in a list.
+#
+# Example:
+# Input:  nums = [1, 3, 2, 6, 4, 8], k = 3
+# Output: [2.0, 3.67, 4.0, 6.0]
+#
+# Explanation:
+# Window 1: [1, 3, 2] → sum=6  → avg=2.0
+# Window 2: [3, 2, 6] → sum=11 → avg=3.67
+# Window 3: [2, 6, 4] → sum=12 → avg=4.0
+# Window 4: [6, 4, 8] → sum=18 → avg=6.0
+# ============================================
+# INPUT: list of nums and a window size k
+# OUTPUT: a list of average from each window sum
+# OPERATION: compare (primary) --> find --> collect 
+# PATTERN: sliding window
+# DS: slidind window
+# PLAN:
+#   1. create current_sum = 0, max_sum = 0, result = []
+#   2. loop from 0 to k:
+#   3.   max_sum = current_sum
+#   4.   result.append(max_sum/k)
+#   5. loop from k to len(nums)
+#        current_sum += nums[i]
+#        current_sum -= nums[i-k]
+#        max_sum = max(max_sum, current_sum)
+#        result.append(max_sum/k)
+#
+nums = [1, 3, 2, 6, 4, 8]
+k = 3
+
+def subarray_averages(nums, k):
+    current_sum = 0
+    max_sum = 0
+    result = []
+    for i in range(k):
+        
+
+print(subarray_averages(nums, k))
